2a6b88359ecbe5b3b67fe789be25b6ece52f2e9d
Rampraveeen <boxerpraveen16@gmail.com>
2022-04-14 20:49:11 +0530

    'changes'

--- src/Components/AuthConfig/AuthForm.js
+++ src/Components/AuthConfig/AuthForm.js
@@ -1,16 +1,99 @@
-import React from "react";
+import React, { useState } from "react";
 import InputField from "../InputField";
 import PrimaryButton from "../PrimaryButton";
 import "./AuthForm.css";
 
 const AuthForm = () => {
-  const handleChange = (event) => {
-    console.log(event.target.value);
-  };
+  const [formValid, setFormValid] = useState(false);
+  const [inputs, setInputs] = useState({
+    name: { value: "", isValid: false },
+    password: { value: "", isValid: false },
+    confirmPassword: { value: "", isValid: false },
+    email: { value: "", isValid: false },
+    phone: { value: "", isValid: false },
+  });
+
+  function inputChangedHandler(inputIdentifier, enteredValue) {
+    console.log(inputIdentifier, enteredValue.target.value);
+    setInputs((curInputs) => {
+      return {
+        ...curInputs,
+        [inputIdentifier]: { value: enteredValue.target.value, isValid: true },
+      };
+    });
+  }
 
   const onSubmit = (e) => {
     e.preventDefault();
-    console.log("form submitted");
+
+    const signUpData = {
+      name: inputs.name.value,
+      password: inputs.password.value,
+      confirmPassword: inputs.confirmPassword.value,
+      email: inputs.email.value,
+      phone: inputs.phone.value,
+    };
+
+    const passLengthValidation = (value) => {
+      const regEx = new RegExp(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/);
+      return regEx.test(value);
+    };
+
+    const emailValidation = (value) => {
+      const regEx = new RegExp(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,20}$/);
+      return regEx.test(value);
+    };
+
+    const nameIsValid = signUpData.name.trim().length > 0;
+    const passwordIsValid = passLengthValidation(signUpData.password.trim());
+    const confirmPasswordIsValid =
+      signUpData.password === signUpData.confirmPassword &&
+      passLengthValidation(signUpData.confirmPassword.trim());
+    const emailIsValid = emailValidation(signUpData.email);
+    const phoneIsValid =
+      !isNaN(signUpData.phone) && signUpData.phone.length === 10;
+
+    if (
+      nameIsValid ||
+      passwordIsValid ||
+      emailIsValid ||
+      confirmPasswordIsValid ||
+      phoneIsValid
+    ) {
+      setInputs((curInputs) => {
+        return {
+          name: {
+            value: curInputs.name.value,
+            isValid: nameIsValid,
+          },
+          password: {
+            value: curInputs.password.value,
+            isValid: passwordIsValid,
+          },
+          confirmPassword: {
+            value: curInputs.confirmPassword.value,
+            isValid: confirmPasswordIsValid,
+          },
+          email: {
+            value: curInputs.email.value,
+            isValid: emailIsValid,
+          },
+          phone: {
+            value: curInputs.phone.value,
+            isValid: phoneIsValid,
+          },
+        };
+      });
+    }
+    if (
+      nameIsValid &&
+      passwordIsValid &&
+      confirmPasswordIsValid &&
+      emailIsValid &&
+      phoneIsValid
+    ) {
+      setFormValid(true);
+    }
   };
 
   return (
@@ -19,39 +102,51 @@ const AuthForm = () => {
         label="Name"
         type="text"
         name="Name"
-        onChange={handleChange}
+        onChange={inputChangedHandler.bind(this, "name")}
+        value={inputs.name.value}
+        invalid={inputs.name.isValid}
         required
       />
       <InputField
         label="Password"
         type="password"
         name="Password"
-        onChange={handleChange}
+        onChange={inputChangedHandler.bind(this, "password")}
+        value={inputs.password.value}
+        invalid={inputs.password.isValid}
         required
       />
       <InputField
         label="Confirm Password"
         type="password"
         name="Confirm Password"
-        onChange={handleChange}
+        onChange={inputChangedHandler.bind(this, "confirmPassword")}
+        value={inputs.confirmPassword.value}
+        invalid={inputs.confirmPassword.isValid}
         required
       />
       <InputField
         label="Email Address"
         type="email"
         name="Email Address"
-        onChange={handleChange}
+        onChange={inputChangedHandler.bind(this, "email")}
+        value={inputs.email.value}
+        invalid={inputs.email.isValid}
         required
       />
       <InputField
         label="Phone Number"
         type="tel"
         name="Phone Number"
-        onChange={handleChange}
+        onChange={inputChangedHandler.bind(this, "phone")}
+        value={inputs.phone.value}
+        invalid={inputs.phone.isValid}
         required
       />
       <div className="btnContainer">
-        <PrimaryButton onClick={onSubmit}>Submit</PrimaryButton>
+        <PrimaryButton type="submit" onClick={onSubmit}>
+          Submit
+        </PrimaryButton>
       </div>
     </form>
   );
--- src/Components/InputField.js
+++ src/Components/InputField.js
@@ -1,12 +1,50 @@
 import React from "react";
 import "./InputField.css";
 
-const InputField = ({ label, type, ...otherProps }) => {
+const InputField = ({ label, invalid, type, ...otherProps }) => {
+  let invalidText;
+
+  if (invalid) {
+    if (label === "Name") {
+      invalidText = (
+        <small className="invalidText">Please check your UserName.</small>
+      );
+    }
+    if (label === "Password") {
+      invalidText = (
+        <small className="invalidText">
+          Password should between 6 to 20 characters which contain at least 1
+          numeric digit, 1 uppercase and 1 lowercase letter
+        </small>
+      );
+    }
+    if (label === "Confirm Password") {
+      invalidText = (
+        <small className="invalidText">Password should be same</small>
+      );
+    }
+    if (label === "Email Address") {
+      invalidText = (
+        <small className="invalidText">
+          Please enter your valid Email Address.
+        </small>
+      );
+    }
+    if (label === "Phone Number") {
+      invalidText = (
+        <small className="invalidText">
+          Please enter your valid Phone Number.
+        </small>
+      );
+    }
+  }
+
   return (
     <div className="inputContainer">
       <label className="label">{label}</label>
       <br />
       <input className="input" type={type} {...otherProps} />
+      {invalidText}
     </div>
   );
 };
